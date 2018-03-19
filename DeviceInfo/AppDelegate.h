cdbc10f28b6b30bc3e8f7cbb6c25a7be1d266172
Reason Lee <linruigongzi@163.com>
2018-03-19 16:50:50 +0800

    初始化项目

---
+++ DeviceInfo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DeviceInfo
+//
+//  Created by Reason Lee on 19/03/2018.
+//  Copyright © 2018 Reason Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
