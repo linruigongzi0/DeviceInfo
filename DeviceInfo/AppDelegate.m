95a8ab7aa73fa3c02f86617f72a47cda79f3009c
Reason Lee <linruigongzi@163.com>
2018-05-16 09:54:17 +0800

    original

--- DeviceInfo/AppDelegate.m
+++ DeviceInfo/AppDelegate.m
@@ -17,18 +17,12 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-
-    NSLog(@"Rebase 1");
-    
-    NSLog(@"master 1");
     
     return YES;
 }
 
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-    
-    NSLog(@"master 2");
     
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
@@ -38,8 +32,6 @@
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    
-    NSLog(@"Rebase 2");
 }
 
 
