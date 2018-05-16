7baa12e98aff173c2b39a408fd12bfbada107fe1
Reason Lee <linruigongzi@163.com>
2018-05-16 14:46:02 +0800

    Feature start

--- DeviceInfo/AppDelegate.m
+++ DeviceInfo/AppDelegate.m
@@ -18,6 +18,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
+    NSLog(@"Feature Start");
+    
     return YES;
 }
 
