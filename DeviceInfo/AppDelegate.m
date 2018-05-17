e3b3fcf971eaca7cb780d02e219625166fda5873
Reason Lee <linruigongzi@163.com>
2018-05-17 10:10:38 +0800

    no message

--- DeviceInfo/AppDelegate.m
+++ DeviceInfo/AppDelegate.m
@@ -18,17 +18,11 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    NSLog(@"Master");
-    
     return YES;
 }
 
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-    
-    NSLog(@"Feature Begin");
-    
-    NSLog(@"Feature End");
     
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
